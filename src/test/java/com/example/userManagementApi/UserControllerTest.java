478a83e3feaf0fa41d56b13ddfc5b566b8913bad
Yogii9071 <yogeshtheboy17@gmail.com>
2025-07-04 11:49:52 +0530

    Added user controller and model setup

---
+++ src/test/java/com/example/userManagementApi/UserControllerTest.java
@@ -0,0 +1,30 @@
+package com.example.userManagementApi;
+import com.example.userManagementApi.model.User;
+import com.example.userManagementApi.repository.UserRepository;
+import com.fasterxml.jackson.databind.ObjectMapper;
+import com.fasterxml.jackson.datatype.jsr310.JavaTimeModule;
+import org.junit.jupiter.api.BeforeEach;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.web.servlet.MockMvc;
+
+@SpringBootTest
+@AutoConfigureMockMvc
+public class UserControllerTest {
+    @Autowired
+    private MockMvc mockMvc;
+    @Autowired
+    private ObjectMapper objectMapper;
+    @Autowired
+    private UserRepository userRepository;
+
+    private User user1;
+    private User user2;
+
+    @BeforeEach
+    void setUp(){
+
+    }
+
+}
