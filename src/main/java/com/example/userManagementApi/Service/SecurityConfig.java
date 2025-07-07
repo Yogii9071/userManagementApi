9f99070ccbde1fd4e73f1de150f8c2c0820c8db2
Yogii9071 <yogeshtheboy17@gmail.com>
2025-07-07 14:28:50 +0530

    service and security

---
+++ src/main/java/com/example/userManagementApi/Service/SecurityConfig.java
@@ -0,0 +1,79 @@
+package com.example.userManagementApi.Service;
+
+import com.example.userManagementApi.model.User;
+import jakarta.validation.Valid;
+import com.example.userManagementApi.repository.UserRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.annotation.Bean;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.security.authentication.AuthenticationProvider;
+import org.springframework.security.authentication.dao.DaoAuthenticationProvider;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configurers.AbstractHttpConfigurer;
+import org.springframework.security.config.http.SessionCreationPolicy;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.security.web.SecurityFilterChain;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.cors.CorsConfiguration;
+import org.springframework.web.cors.CorsConfigurationSource;
+import org.springframework.web.cors.UrlBasedCorsConfigurationSource;
+
+import java.util.Collections;
+import java.util.List;
+import java.util.Optional;
+
+public class SecurityConfig {
+
+    // Inject your UserDetailsService (e.g., UserDetail class)
+    @Autowired
+    private UserRepository userRepository;
+    @Bean
+    public UserDetailsService userDetailsService() {
+        return new UserDetail(); // your custom class implementing UserDetailsService
+    }
+
+    @Bean
+    public AuthenticationProvider authenticationProvider() {
+        DaoAuthenticationProvider authProvider = new DaoAuthenticationProvider();
+        authProvider.setUserDetailsService(userDetailsService()); // use correct method
+        authProvider.setPasswordEncoder(passwordEncoder());
+        return authProvider;
+    }
+
+    @Bean
+    public PasswordEncoder passwordEncoder() {
+        return new BCryptPasswordEncoder();
+    }
+
+    public CorsConfigurationSource corsConfigurationSource() {
+        CorsConfiguration config = new CorsConfiguration();
+        config.setAllowedOrigins(List.of("*"));
+        config.setAllowedMethods(List.of("GET", "POST", "PUT", "DELETE", "OPTIONS"));
+        config.setAllowedHeaders(List.of("*"));
+        config.setAllowCredentials(true);
+
+        UrlBasedCorsConfigurationSource source = new UrlBasedCorsConfigurationSource();
+
+        source.registerCorsConfiguration("/**", config);
+        return  source;
+    }
+    @Bean
+    public SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
+        http.cors(cors->cors.configurationSource(corsConfigurationSource()))
+                .csrf(AbstractHttpConfigurer::disable).authorizeHttpRequests(auth -> auth
+                        .requestMatchers("api/users/register").permitAll()
+                        .requestMatchers("api/users/page").permitAll()
+                        .requestMatchers("api/users").hasRole("ADMIN")
+                        .requestMatchers("api/users").hasAnyRole("USER","ADMIN")
+                        .anyRequest().authenticated()).httpBasic(org.springframework.security.config.Customizer.withDefaults())
+                .sessionManagement(session->session.sessionCreationPolicy(SessionCreationPolicy.STATELESS));
+        return http.build();
+    }
+
+}
---
+++ src/test/java/com/example/userManagementApi/UserRepositoryTest.java
@@ -0,0 +1,4 @@
+package com.example.userManagementApi;
+
+public class UserRepositoryTest {
+}
